bb1210081e5d2eeaa077aaf1432248a29a87d5dc
rohit <rohit.intersoft@gmail.com>
2020-05-23 11:04:51 +0530

    first commit

---
+++ BehaviorApp/BehaviorApp/MainPage.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BehaviorApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public static bool IsNumericValid;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void Entry_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        public void Submit(object sender, EventArgs e)
+        {
+            DisplayAlert("Notice", "Data Submitted Successfully", "Ok");
+        }
+    }
+
+    public class NumericValidation : Behavior<Entry>
+    {
+        public static readonly BindableProperty IsValidProperty =
+            BindableProperty.Create(nameof(IsValid), typeof(bool),
+                typeof(NumericValidation), false, BindingMode.OneWayToSource);
+
+        public bool IsValid
+        {
+            get { return (bool)GetValue(IsValidProperty); }
+            set { SetValue(IsValidProperty, value); }
+        }
+
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+           
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            double result;
+            bool isValid = double.TryParse(args.NewTextValue, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+            IsValid = isValid;
+            
+        }
+    }
+
+    public class RequiredValidatorBehavior : Behavior<Entry>
+    {
+        public static readonly BindableProperty IsValidProperty =
+            BindableProperty.Create(nameof(IsValid), typeof(bool),
+                typeof(NumericValidation), false, BindingMode.OneWayToSource);
+
+        public bool IsValid
+        {
+            get { return (bool)GetValue(IsValidProperty); }
+            set { SetValue(IsValidProperty, value); }
+        }
+
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            //double result;
+            bool isValid = (args.NewTextValue.Length > 0);
+            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+            IsValid = isValid;
+
+        }
+    }
+
+    public class EmailValidation : Behavior<Entry>
+    {
+        public static readonly BindableProperty IsValidProperty2 =
+            BindableProperty.Create(nameof(IsValid2), typeof(bool),
+                typeof(NumericValidation), false, BindingMode.OneWayToSource);
+
+        public bool IsValid2
+        {
+            get { return (bool)GetValue(IsValidProperty2); }
+            set { SetValue(IsValidProperty2, value); }
+        }
+
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+
+        }
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            //double result;
+            bool isValid = Regex.IsMatch(args.NewTextValue, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+            //bool isValid = double.TryParse(args.NewTextValue, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+            IsValid2 = isValid;
+
+        }
+
+    }
+
+    public class ConfirmPasswordBehavior : Behavior<Entry>
+    {
+        static readonly BindablePropertyKey IsValidPropertyKey = BindableProperty.CreateReadOnly("IsValid", typeof(bool), typeof(ConfirmPasswordBehavior), false);
+        public static readonly BindableProperty IsValidProperty = IsValidPropertyKey.BindableProperty;
+
+        public static readonly BindableProperty CompareToEntryProperty = BindableProperty.Create("CompareToEntry", typeof(Entry), typeof(ConfirmPasswordBehavior), null);
+
+        public Entry CompareToEntry
+        {
+            get { return (Entry)base.GetValue(CompareToEntryProperty); }
+            set { base.SetValue(CompareToEntryProperty, value); }
+        }
+        public bool IsValid
+        {
+            get { return (bool)base.GetValue(IsValidProperty); }
+            private set { base.SetValue(IsValidPropertyKey, value); }
+        }
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += HandleTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= HandleTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+        void HandleTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var password = CompareToEntry.Text;
+            var confirmPassword = e.NewTextValue;
+            IsValid = password.Equals(confirmPassword);
+        }
+    }
+}
